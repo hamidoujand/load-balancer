4fb716d51b812974171d1547cffd4a74ea57bda3
Hamid Oujand <hmdrzadev@gmail.com>
2025-02-03 01:38:21 +0330

    add Backend type

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hamidoujand/load-balancer
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import "sync"
+
+func main() {
+
+}
+
+// ==============================================================================
+// Backend
+type Backend struct {
+	URL          string
+	Healthy      bool
+	mu           sync.RWMutex
+	failureCount int
+}
+
+func (b *Backend) IsHealthy() bool {
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+	return b.Healthy
+}
+
+func (b *Backend) MarkHealthy() {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	b.Healthy = true
+	b.failureCount = 0
+}
+
+func (b *Backend) MarkUnHealthy() {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	b.Healthy = false
+}
+
+func (b *Backend) IncrementFailure() {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	b.failureCount++
+	if b.failureCount >= 5 {
+		b.Healthy = false
+	}
+}
+
+//==============================================================================
